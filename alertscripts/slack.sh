7e0045650754db42381105402aadd235125b55fb
Benj Dag <bjdag1234@gmail.com>
2017-06-14 07:42:31 +0800

    added more cleaning

--- alertscripts/slack.sh
+++ alertscripts/slack.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo "$@" | grep -v UNKNOWN | sed -i 's/server-admins@dream.upd.edu.ph //g' | slacktee.sh -e "Date and Time" "$(date)" -u "zabbix01" -a "good" -o "danger" "^PROBLEM"
+echo "$@" | grep -v UNKNOWN | sed 's/server-admins@dream.upd.edu.ph //g' | slacktee.sh -e "Date and Time" "$(date)" -u "zabbix01" -a "good" -o "danger" "^PROBLEM"
