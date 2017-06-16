3e8041c074e8da316bc92333a29a7416db0945a1
Kenneth Langga <klangga@gmail.com>
2017-06-16 13:07:45 +0800

    Fixed sed error in update_conf.sh

--- update_conf.sh
+++ update_conf.sh
@@ -7,5 +7,5 @@ fi
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-sed -i "s/<HOSTNAME>/$(hostname -s)/g" $1
-sed -i "s/<SCRIPT_DIR>/${SCRIPT_DIR}/g" $1
+sed -i '' "s/<HOSTNAME>/$(hostname -s)/g" $1
+sed -i '' "s/<SCRIPT_DIR>/${SCRIPT_DIR//\//\\/}/g" $1
